6073f890022210dd888bd90f6f95404ca26ee977
mgutz <mario.l.gutierrez@gmail.com>
2020-01-13 20:46:43 +0000

    do not use go or node in mini install

--- install.sh
+++ install.sh
@@ -3,19 +3,18 @@
 __dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 if [ "$1" == "full" ]; then
-	cp Plugfile.full Plugfile
+    for cmd in node npm go; do
+        command -v $cmd >/dev/null 2>&1 || {
+            echo $cmd is required not installed
+            exit 1
+        }
+    done
+    cp Plugfile.full Plugfile
 else
-	cp Plugfile.mini Plugfile
+    cp Plugfile.mini Plugfile
 fi
 
 # ensure pre-requisites are installed
-for cmd in node npm go; do
-	command -v $cmd >/dev/null 2>&1 || {
-		echo $cmd is required not installed
-		exit 1
-	}
-done
-
 # backup directory
 mkdir -p ./_backups
 
@@ -27,7 +26,7 @@ mkdir -p ~/.config/nvim
 
 # download vim-plug manager
 curl -fLo _plugins/plug.vim \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # coc.nvim expects known location
 ln -sf $PWD/coc-settings.json ~/.config/nvim/coc-settings.json
