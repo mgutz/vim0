2ae0c7cbb6163a81dcd8c105765c9e41eafddb84
mgutz <mario@matcherino.com>
2020-01-15 09:57:36 +0800

    check for nvim

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 __dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 if [ "$1" == "full" ]; then
-    for cmd in node npm go; do
-        command -v $cmd >/dev/null 2>&1 || {
-            echo $cmd is required not installed
-            exit 1
-        }
-    done
-    cp Plugfile.full Plugfile
+	for cmd in nvim node npm go; do
+		command -v $cmd >/dev/null 2>&1 || {
+			echo $cmd is required not installed
+			exit 1
+		}
+	done
+	cp Plugfile.full Plugfile
 else
-    cp Plugfile.mini Plugfile
+	cp Plugfile.mini Plugfile
 fi
 
 # ensure pre-requisites are installed
@@ -21,23 +21,29 @@ mkdir -p ./_backups
 # plugins directory
 mkdir -p ./_plugins
 
-# nvim configuration directory
-mkdir -p ~/.config/nvim
-
 # download vim-plug manager
 curl -fLo _plugins/plug.vim \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# coc.nvim expects known location
-ln -sf $PWD/coc-settings.json ~/.config/nvim/coc-settings.json
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # hook into vim
 echo "source $__dirname/vimrc" >~/.vimrc
 
-# hook nvim
-mkdir -p ~/.config/nvim
-echo "
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source $__dirname/vimrc
-" >~/.config/nvim/init.vim
+if command -v nvim >/dev/null; then
+	# nvim configuration directory
+	mkdir -p ~/.config/nvim
+
+	# coc.nvim expects known location
+	ln -sf $PWD/coc-settings.json ~/.config/nvim/coc-settings.json
+
+	# hook nvim
+	mkdir -p ~/.config/nvim
+	echo "
+	set runtimepath^=~/.vim runtimepath+=~/.vim/after
+	let &packpath = &runtimepath
+	source $__dirname/vimrc
+	" >~/.config/nvim/init.vim
+
+	nvim +PlugInstall +qall
+else
+	vim +PlugInstall +qall
+fi
